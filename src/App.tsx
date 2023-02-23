a845eda7df8c77d943292da2862d3b1592013283
zmarc3ll <zubermarcellpetrik@gmail.com>
2023-02-23 17:49:17 +0100

    adding interface for user + state

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface User{
+  id: number;
+  username: string;
+  password: string;
+  passwordAuth: string;
+  email: string;
+  birthDate: Date;
 }
+
+interface UserListResponse{
+  users: User[];
+}
+
+interface State{
+  users: User[];
+  usernameInput: string;
+  passwordInput: string;
+  passwordAuthInput: string;
+  emailInput: string;
+  birthDateInput: string; //temporarily string but actually Date
+}
+
 
 export default App;
