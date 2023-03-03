675b464751f8ab321509f450a1d18e8ca860effb
PfefferBotond <botond.pfeffer@gmail.com>
2023-03-03 10:46:22 +0100

    log in form done

--- src/Login.tsx
+++ src/Login.tsx
@@ -81,72 +81,8 @@ export default class Login extends Component {
                         </div>
                     </div>
                 </div>
-            </section>
-            <footer className="text-center text-lg-start bg-light text-muted footer">
-            <section className="">
-              <div className="container text-center text-md-start mt-5">
-                <div className="row mt-3">
-                  <div className="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
-                    <h6 className="text-uppercase fw-bold mb-4">
-                      <i className="fas fa-gem me-3"></i>TeAutod.hu
-                    </h6>
-                    <p>
-                      Here you can use rows and columns to organize your footer content. Lorem ipsum
-                      dolor sit amet, consectetur adipisicing elit.
-                    </p>
-                  </div>
-                  <div className="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
-                    <h6 className="text-uppercase fw-bold mb-4">
-                      Oldalak
-                    </h6>
-                    <p>
-                      <a href="#!" className="text-reset">Főoldal</a>
-                    </p>
-                    <p>
-                      <a href="#!" className="text-reset">Garázs</a>
-                    </p>
-                    <p>
-                      <a href="#!" className="text-reset">Naptár</a>
-                    </p>
-                    <p>
-                      <a href="#!" className="text-reset">Rólunk</a>
-                    </p>
-                  </div>
-                  <div className="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
-                    <h6 className="text-uppercase fw-bold mb-4">
-                      Useful links
-                    </h6>
-                    <p>
-                      <a href="#!" className="text-reset">Pricing</a>
-                    </p>
-                    <p>
-                      <a href="#!" className="text-reset">Settings</a>
-                    </p>
-                    <p>
-                      <a href="#!" className="text-reset">Orders</a>
-                    </p>
-                    <p>
-                      <a href="#!" className="text-reset">Help</a>
-                    </p>
-                  </div>
-                  <div className="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
-                    <h6 className="text-uppercase fw-bold mb-4">Kapcsolat</h6>
-                    <p><i className="fas fa-home me-3"></i> Budapest, 14.kerület, HU</p>
-                    <p>
-                      <i className="fas fa-envelope me-3"></i>
-                      teautod@gmail.com
-                    </p>
-                    <p><i className="fas fa-phone me-3"></i> + 01 234 567 88</p>
-                    <p><i className="fas fa-print me-3"></i> + 01 234 567 89</p>
-                  </div>
-                </div>
-              </div>
-            </section>
-            <div className="text-center p-4">
-              © 2023 Copyright:
-              <a className="text-reset fw-bold" href="#">TeAutod.hu</a>
-            </div>
-          </footer></>
+            </section></>
+        
     }
     
 }
