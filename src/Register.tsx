21d6fe1da5af7e2171dbdd432e2f006d96288a94
Zuber Marcell <zubermarcellpetrik@gmail.com>
2023-03-09 18:04:24 +0100

    required filds

--- src/Register.tsx
+++ src/Register.tsx
@@ -96,51 +96,54 @@ export default class Register extends Component<{}, State> {
                                 <div className="row justify-content-center">
                                     <div className="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
                                         <p className="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">Regisztráció</p>
-                                        <form className="mx-1 mx-md-4">
+                                        <form className="mx-1 mx-md-4 needs-validation" noValidate>
                                             <div className="d-flex flex-row align-items-center mb-4">
                                                 <i className="fas fa-user fa-lg me-3 fa-fw"></i>
                                                 <div className="form-outline flex-fill mb-0">
                                                 <label className="form-label" htmlFor="form3Example1c">Felhasználónév</label>
-                                                    <input type="text" id="form3Example1c" className="form-control" value={usernameInput} onChange={e => this.setState({ usernameInput: e.currentTarget.value })} />
+                                                    <input type="text" id="form3Example1c" className="form-control" value={usernameInput} required onChange={e => this.setState({ usernameInput: e.currentTarget.value })} />
+                                                    <label htmlFor="form3Example1c">error</label>
                                                 </div>
                                             </div>
                                             <div className="d-flex flex-row align-items-center mb-4">
                                                 <i className="fas fa-envelope fa-lg me-3 fa-fw"></i>
                                                 <div className="form-outline flex-fill mb-0">
                                                     <label className="form-label" htmlFor="form3Example3c">Email</label>
-                                                    <input type="email" id="form3Example3c" className="form-control" value={emailInput} onChange={e => this.setState({ emailInput: e.currentTarget.value })} />
+                                                    <input type="email" id="form3Example3c" className="form-control" value={emailInput} required onChange={e => this.setState({ emailInput: e.currentTarget.value })} />
                                                 </div>
                                             </div>
                                             <div className="d-flex flex-row align-items-center mb-4">
                                                 <i className="fas fa-lock fa-lg me-3 fa-fw"></i>
                                                 <div className="form-outline flex-fill mb-0">
                                                     <label className="form-label" htmlFor="form3Example4c">Jelszó</label>
-                                                    <input type="password" id="form3Example4c" className="form-control" value={passwordInput} onChange={e => this.setState({ passwordInput: e.currentTarget.value })} />
+                                                    <input type="password" id="form3Example4c" className="form-control" value={passwordInput} required onChange={e => this.setState({ passwordInput: e.currentTarget.value })} />
                                                 </div>
                                             </div>
                                             <div className="d-flex flex-row align-items-center mb-4">
                                                 <i className="fas fa-key fa-lg me-3 fa-fw"></i>
                                                 <div className="form-outline flex-fill mb-0">
                                                     <label className="form-label" htmlFor="form3Example4cd">Jelszó újra</label>
-                                                    <input type="password" id="form3Example4cd" className="form-control" value={passwordAuthInput} onChange={e => this.setState({ passwordAuthInput: e.currentTarget.value })} />
+                                                    <input type="password" id="form3Example4cd" className="form-control" value={passwordAuthInput} required onChange={e => this.setState({ passwordAuthInput: e.currentTarget.value })} />
                                                 </div>
                                             </div>
                                             <div className="d-flex flex-row align-items-center mb-4">
                                                 <i className="fas fa-key fa-lg me-3 fa-fw"></i>
                                                 <div className="form-outline flex-fill mb-0">
                                                     <label className="form-label" htmlFor="form3Example4cd">Születési dátum</label>
-                                                    <input type="date" id="form3Example4cd" className="form-control" value={birthDateInput} onChange={e => this.setState({ birthDateInput: e.currentTarget.value })} />
+                                                    <input type="date" id="form3Example4cd" className="form-control" value={birthDateInput} required onChange={e => this.setState({ birthDateInput: e.currentTarget.value })} />
                                                 </div>
                                             </div>
 
                                             <div className="form-check d-flex justify-content-center mb-5">
-                                                <input className="form-check-input me-2" type="checkbox" value="" id="form2Example3c" />
+                                                <input className="form-check-input me-2" type="checkbox" value="" required id="form2Example3c" />
                                                 <label className="form-check-label" htmlFor="form2Example3">
                                                     Elfogadom a felhasználói feltételeket ( <a href="./tos.html">Terms of service</a> )
                                                 </label>
                                             </div>
                                             <div className="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
-                                                <Link to={'/login'}><button type="submit" className="btn btn-success btn-lg" onClick={this.handleUpload}>Regisztrálás</button></Link>
+                                                {/* <Link to={'/login'}> */}
+                                                    <button type="submit" className="btn btn-success btn-lg" onClick={this.handleUpload}>Regisztrálás</button>
+                                                    {/* </Link> */}
                                             </div>
                                             <p className="small fw-bold mt-2 pt-1 mb-0" id="hasAccount">Van már fiókja? <Link to={'/login'} className="link-danger">Bejelentkezés</Link></p>
                                         </form>
