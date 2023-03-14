d767d4ee5aa969eaeb4a16a7a4a021891b0f432d
zmarc3ll <zubermarcellpetrik@gmail.com>
2023-03-14 13:01:47 +0100

    body wrap

--- src/components/pages/Login.tsx
+++ src/components/pages/Login.tsx
@@ -3,48 +3,47 @@ import { Link } from "react-router-dom";
 
 export default class Login extends Component {
     render() {
-        return <><section>
-                <div className="container-fluid h-custom">
-                    <div className="row d-flex justify-content-center align-items-center h-100" id="fillPage">
-                        <div className="col-md-9 col-lg-6 col-xl-5">
-                            <img src={'auto-removebg-preview.png'} className="img-fluid" alt="Sample image" />
-                        </div>
-                        <div className="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
-                            <form>
-                                <div className="divider d-flex align-items-center my-4">
-                                    <h2 className="text-center fw-bold mx-1 mb-3 pt-2">Belépés</h2>
-                                </div>
-
-                                <div className="form-outline mb-4">
-                                    <input type="text" id="form3Example3" className="form-control form-control-lg"
-                                        placeholder="Adja meg a felhsználónevét" />
-                                    <label className="form-label" htmlFor="form3Example3">Felhasználónév</label>
-                                </div>
-
-                                <div className="form-outline mb-3">
-                                    <input type="password" id="form3Example4" className="form-control form-control-lg"
-                                        placeholder="Adja meg a jelszót" />
-                                    <label className="form-label" htmlFor="form3Example4">Jelszó</label>
-                                </div>
-
-                                <div className="d-flex justify-content-between align-items-center">
-                                    <div className="form-check mb-0">
-                                        <input className="form-check-input me-2" type="checkbox" value="" id="form2Example3" />
-                                        <label className="form-check-label" htmlFor="form2Example3">
-                                            Emlékezz rám
-                                        </label>
+        return <><body id="undoBlockContent">
+            <section>
+                    <div className="container-fluid h-custom">
+                        <div className="row d-flex justify-content-center align-items-center h-100" id="fillPage">
+                            <div className="col-md-9 col-lg-6 col-xl-5">
+                                <img src={'auto-removebg-preview.png'} className="img-fluid" alt="Sample image" />
+                            </div>
+                            <div className="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
+                                <form>
+                                    <div className="divider d-flex align-items-center my-4">
+                                        <h2 className="text-center fw-bold mx-1 mb-3 pt-2">Belépés</h2>
                                     </div>
-                                    <p className="text-body"><em>Üdvözöljük!</em></p>
-                                </div>
-                                <div className="text-center text-lg-start mt-4 pt-2">
-                                    <button className="btn btn-success btn-lg" type="submit" role="button" aria-pressed="false">Bejelentkezés</button>
-                                    <p className="small fw-bold mt-2 pt-1 mb-0">Nincs fiókja? <Link to={'/register'} className="link-danger">Regisztrálok</Link></p>
-                                </div>
-                            </form>
+                                    <div className="form-outline mb-4">
+                                        <input type="text" id="form3Example3" className="form-control form-control-lg"
+                                            placeholder="Adja meg a felhsználónevét" />
+                                        <label className="form-label" htmlFor="form3Example3">Felhasználónév</label>
+                                    </div>
+                                    <div className="form-outline mb-3">
+                                        <input type="password" id="form3Example4" className="form-control form-control-lg"
+                                            placeholder="Adja meg a jelszót" />
+                                        <label className="form-label" htmlFor="form3Example4">Jelszó</label>
+                                    </div>
+                                    <div className="d-flex justify-content-between align-items-center">
+                                        <div className="form-check mb-0">
+                                            <input className="form-check-input me-2" type="checkbox" value="" id="form2Example3" />
+                                            <label className="form-check-label" htmlFor="form2Example3">
+                                                Emlékezz rám
+                                            </label>
+                                        </div>
+                                        <p className="text-body"><em>Üdvözöljük!</em></p>
+                                    </div>
+                                    <div className="text-center text-lg-start mt-4 pt-2">
+                                        <button className="btn btn-success btn-lg" type="submit" role="button" aria-pressed="false">Bejelentkezés</button>
+                                        <p className="small fw-bold mt-2 pt-1 mb-0">Nincs fiókja? <Link to={'/register'} className="link-danger">Regisztrálok</Link></p>
+                                    </div>
+                                </form>
+                            </div>
                         </div>
                     </div>
-                </div>
-            </section></>
+                </section>
+        </body></>
         
     }
     
--- src/components/pages/Register.tsx
+++ src/components/pages/Register.tsx
@@ -132,96 +132,95 @@ export default class Register extends Component<{}, State> {
         const usernameReg = /^[a-zA-Z0-9]{3,10}$/;
         let isValid;
         return <>
-            <section className="vh-200">
-                <div className="container h-100">
-                    <div className="row d-flex justify-content-center align-items-center h-100">
-                        <div className="col-lg-12 col-xl-11">
-                            <div className="card text-black">
-                                <div className="card-body p-md-5">
-                                    <div className="row justify-content-center">
-                                        <div className="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
-                                            <p className="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">Regisztráció</p>
-                                            <> {(() => {
-                                                    if (usernameWrong === '' || emailWrong === '' || passwordWrong === '' || passwordAuthWrong === '' || birthDateWrong === '') {
-                                                        isValid = true
-                                                    } else {
-                                                        isValid = false;
-                                                    }
-                                                })()}</>
-                                            <form className="mx-1 mx-md-4" onSubmit={this.handleUpload}>
-                                                <div className="d-flex flex-row align-items-center mb-4">
-                                                    <i className="fas fa-user fa-lg me-3 fa-fw"></i>
-                                                    <div className="form-outline flex-fill mb-0">
-                                                        <label className="form-label" htmlFor="form3Example1c">Felhasználónév</label>
-                                                        <input type="text" id="form3Example1c" required className="form-control" value={usernameInput} onChange={
-                                                            e => this.setState({ usernameInput: e.currentTarget.value })} />
-                                                        {!usernameReg.test(usernameInput) ? (
-                                                            <label htmlFor="form3Example1c" className="form-label label-valid">
-                                                                {usernameWrong}
-                                                            </label>
-                                                        ) : (
-                                                            <img className="checkmark" src={'check-mark.png'} />
-                                                        )}
+            <body id="undoBlockContent">
+                <section className="vh-200">
+                    <div className="container h-100">
+                        <div className="row d-flex justify-content-center align-items-center h-100">
+                            <div className="col-lg-12 col-xl-11">
+                                <div className="card text-black">
+                                    <div className="card-body p-md-5">
+                                        <div className="row justify-content-center">
+                                            <div className="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
+                                                <p className="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">Regisztráció</p>
+                                                <> {(() => {
+                                                        if (usernameWrong === '' || emailWrong === '' || passwordWrong === '' || passwordAuthWrong === '' || birthDateWrong === '') {
+                                                            isValid = true
+                                                        } else {
+                                                            isValid = false;
+                                                        }
+                                                    })()}</>
+                                                <form className="mx-1 mx-md-4" onSubmit={this.handleUpload}>
+                                                    <div className="d-flex flex-row align-items-center mb-4">
+                                                        <i className="fas fa-user fa-lg me-3 fa-fw"></i>
+                                                        <div className="form-outline flex-fill mb-0">
+                                                            <label className="form-label" htmlFor="form3Example1c">Felhasználónév</label>
+                                                            <input type="text" id="form3Example1c" required className="form-control" value={usernameInput} onChange={
+                                                                e => this.setState({ usernameInput: e.currentTarget.value })} />
+                                                            {!usernameReg.test(usernameInput) ? (
+                                                                <label htmlFor="form3Example1c" className="form-label label-valid">
+                                                                    {usernameWrong}
+                                                                </label>
+                                                            ) : (
+                                                                <img className="checkmark" src={'check-mark.png'} />
+                                                            )}
+                                                        </div>
                                                     </div>
-                                                </div>
-                                                <div className="d-flex flex-row align-items-center mb-4">
-                                                    <i className="fas fa-envelope fa-lg me-3 fa-fw"></i>
-                                                    <div className="form-outline flex-fill mb-0">
-                                                        <label className="form-label" htmlFor="form3Example3c">Email</label>
-                                                        <input type="email" id="form3Example3c" className="form-control" value={emailInput} required onChange={e => this.setState({ emailInput: e.currentTarget.value })} />
-                                                        {!emailReg.test(emailInput) ? <label htmlFor="form3Example1c" className="form-label label-valid">{emailWrong}</label> : <img className="checkmark" src={'check-mark.png'} />}
+                                                    <div className="d-flex flex-row align-items-center mb-4">
+                                                        <i className="fas fa-envelope fa-lg me-3 fa-fw"></i>
+                                                        <div className="form-outline flex-fill mb-0">
+                                                            <label className="form-label" htmlFor="form3Example3c">Email</label>
+                                                            <input type="email" id="form3Example3c" className="form-control" value={emailInput} required onChange={e => this.setState({ emailInput: e.currentTarget.value })} />
+                                                            {!emailReg.test(emailInput) ? <label htmlFor="form3Example1c" className="form-label label-valid">{emailWrong}</label> : <img className="checkmark" src={'check-mark.png'} />}
+                                                        </div>
                                                     </div>
-                                                </div>
-                                                <div className="d-flex flex-row align-items-center mb-4">
-                                                    <i className="fas fa-lock fa-lg me-3 fa-fw"></i>
-                                                    <div className="form-outline flex-fill mb-0">
-                                                        <label className="form-label" htmlFor="form3Example4c">Jelszó</label>
-                                                        <input type="password" id="form3Example4c" className="form-control" value={passwordInput} required onChange={e => this.setState({ passwordInput: e.currentTarget.value })} />
-                                                        {passwordInput.length < 6 ? <label htmlFor="form3Example1c" className="form-label label-valid">{passwordWrong}</label> : <img className="checkmark" src={'check-mark.png'} />}
+                                                    <div className="d-flex flex-row align-items-center mb-4">
+                                                        <i className="fas fa-lock fa-lg me-3 fa-fw"></i>
+                                                        <div className="form-outline flex-fill mb-0">
+                                                            <label className="form-label" htmlFor="form3Example4c">Jelszó</label>
+                                                            <input type="password" id="form3Example4c" className="form-control" value={passwordInput} required onChange={e => this.setState({ passwordInput: e.currentTarget.value })} />
+                                                            {passwordInput.length < 6 ? <label htmlFor="form3Example1c" className="form-label label-valid">{passwordWrong}</label> : <img className="checkmark" src={'check-mark.png'} />}
+                                                        </div>
                                                     </div>
-                                                </div>
-                                                <div className="d-flex flex-row align-items-center mb-4">
-                                                    <i className="fas fa-key fa-lg me-3 fa-fw"></i>
-                                                    <div className="form-outline flex-fill mb-0">
-                                                        <label className="form-label" htmlFor="form3Example4cd">Jelszó újra</label>
-                                                        <input type="password" id="form3Example4cd" className="form-control" value={passwordAuthInput} required onChange={e => this.setState({ passwordAuthInput: e.currentTarget.value })} />
-                                                        {passwordAuthInput !== passwordInput ? <label htmlFor="form3Example1c" className="form-label label-valid">{passwordAuthWrong}</label> : (passwordInput ? <img className="checkmark" src={'check-mark.png'} /> : null)}
+                                                    <div className="d-flex flex-row align-items-center mb-4">
+                                                        <i className="fas fa-key fa-lg me-3 fa-fw"></i>
+                                                        <div className="form-outline flex-fill mb-0">
+                                                            <label className="form-label" htmlFor="form3Example4cd">Jelszó újra</label>
+                                                            <input type="password" id="form3Example4cd" className="form-control" value={passwordAuthInput} required onChange={e => this.setState({ passwordAuthInput: e.currentTarget.value })} />
+                                                            {passwordAuthInput !== passwordInput ? <label htmlFor="form3Example1c" className="form-label label-valid">{passwordAuthWrong}</label> : (passwordInput ? <img className="checkmark" src={'check-mark.png'} /> : null)}
+                                                        </div>
                                                     </div>
-                                                </div>
-                                                <div className="d-flex flex-row align-items-center mb-4">
-                                                    <i className="fas fa-key fa-lg me-3 fa-fw"></i>
-                                                    <div className="form-outline flex-fill mb-0">
-                                                        <label className="form-label" htmlFor="form3Example4cd">Születési dátum</label>
-                                                        <input type="date" id="form3Example4cd" className="form-control" value={birthDateInput} required onInput={this.handleValidation} onChange={e => this.setState({ birthDateInput: e.currentTarget.value})} />
-                                                        {new Date(birthDateInput).getFullYear() <= new Date().getFullYear() - 18 ? <img className="checkmark" src={'check-mark.png'} /> : <label htmlFor="form3Example4cd" className="form-label label-valid">{birthDateWrong}</label>}
+                                                    <div className="d-flex flex-row align-items-center mb-4">
+                                                        <i className="fas fa-key fa-lg me-3 fa-fw"></i>
+                                                        <div className="form-outline flex-fill mb-0">
+                                                            <label className="form-label" htmlFor="form3Example4cd">Születési dátum</label>
+                                                            <input type="date" id="form3Example4cd" className="form-control" value={birthDateInput} required onInput={this.handleValidation} onChange={e => this.setState({ birthDateInput: e.currentTarget.value})} />
+                                                            {new Date(birthDateInput).getFullYear() <= new Date().getFullYear() - 18 ? <img className="checkmark" src={'check-mark.png'} /> : <label htmlFor="form3Example4cd" className="form-label label-valid">{birthDateWrong}</label>}
+                                                        </div>
                                                     </div>
-                                                </div>
-
-                                                <div className="form-check d-flex justify-content-center mb-5">
-                                                    <input className="form-check-input me-2" type="checkbox" required value="" id="form2Example3c" onClick={this.handleValidation} />
-                                                    <label className="form-check-label" htmlFor="form2Example3">
-                                                        Elfogadom a felhasználói feltételeket ( <a href="./tos.html">Terms of service</a> )
-                                                    </label>
-                                                </div>
-                                                <div className="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
-                                                   {/* <Link to={'/login'}> */}
-                                                    {isValid ? <button type="submit" className="btn btn-success btn-lg" onClick={this.handleValidation}>Regisztrálás</button> : <button type="submit" disabled className="btn btn-success btn-lg" onClick={this.handleValidation}>Regisztrálás</button>}
-                                                    {/* </Link> */}
-                                                </div>
-
-                                                <p className="small fw-bold mt-2 pt-1 mb-0" id="hasAccount">Van már fiókja? <Link to={'/login'} className="link-danger">Bejelentkezés</Link></p>
-                                            </form>
-
-                                        </div>
-                                        <div className="col-md-10 col-lg-6 col-xl-7 d-flex align-items-center order-1 order-lg-2">
-                                            <img src={'register-splashart.avif'} className="img-fluid" alt="register_plashart" />
+                                                    <div className="form-check d-flex justify-content-center mb-5">
+                                                        <input className="form-check-input me-2" type="checkbox" required value="" id="form2Example3c" onClick={this.handleValidation} />
+                                                        <label className="form-check-label" htmlFor="form2Example3">
+                                                            Elfogadom a felhasználói feltételeket ( <a href="./tos.html">Terms of service</a> )
+                                                        </label>
+                                                    </div>
+                                                    <div className="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
+                                                       {/* <Link to={'/login'}> */}
+                                                        {isValid ? <button type="submit" className="btn btn-success btn-lg" onClick={this.handleValidation}>Regisztrálás</button> : <button type="submit" disabled className="btn btn-success btn-lg" onClick={this.handleValidation}>Regisztrálás</button>}
+                                                        {/* </Link> */}
+                                                    </div>
+                                                    <p className="small fw-bold mt-2 pt-1 mb-0" id="hasAccount">Van már fiókja? <Link to={'/login'} className="link-danger">Bejelentkezés</Link></p>
+                                                </form>
+                                            </div>
+                                            <div className="col-md-10 col-lg-6 col-xl-7 d-flex align-items-center order-1 order-lg-2">
+                                                <img src={'register-splashart.avif'} className="img-fluid" alt="register_plashart" />
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                </div>
-            </section></>
+                </section>
+            </body></>
     }
 }
