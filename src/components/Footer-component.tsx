3085abb294ecb1a32a8af9655d43c92919ccaf7f
zmarc3ll <zubermarcellpetrik@gmail.com>
2023-03-21 18:30:38 +0100

    footer and navbar made into a component

---
+++ src/components/Footer-component.tsx
@@ -0,0 +1,69 @@
+import { Link } from "react-router-dom"
+
+const FooterComponent: React.FC = () => {
+    return (
+        <footer className="text-center text-lg-start text-muted footer">
+        <section className="">
+          <div className="container text-center text-md-start mt-5">
+            <div className="row mt-3">
+              <div className="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+                <h6 className="text-uppercase fw-bold mb-4 text-center">
+                  <i className="fas fa-gem me-3"></i>TeAutod.hu
+                </h6>
+                <Link to={'/'}><img src={'TeAutod.hu.png'} alt="TeAutod.hu" title="TeAutod.hu" className='d-block mx-auto pe-4 pb-3' id='footerLogo' /></Link>
+              </div>
+              <div className="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+                <h6 className="text-uppercase fw-bold mb-4">
+                  Oldalak
+                </h6>
+                <p>
+                  <Link to={'/'} className="text-reset">Főoldal</Link>
+                </p>
+                <p>
+                  <a href="#!" className="text-reset">Garázs</a>
+                </p>
+                <p>
+                  <Link to='./calendar' className="text-reset">Naptár</Link>
+                </p>
+                <p>
+                  <a href="#!" className="text-reset">Rólunk</a>
+                </p>
+              </div>
+              <div className="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+                <h6 className="text-uppercase fw-bold mb-4">
+                  Partnerek
+                </h6>
+                <p>
+                  <a href="#!" className="text-reset">Partner1</a>
+                </p>
+                <p>
+                  <a href="#!" className="text-reset">Partner2</a>
+                </p>
+                <p>
+                  <a href="#!" className="text-reset">Partner3</a>
+                </p>
+                <p>
+                  <a href="#!" className="text-reset">Partner4</a>
+                </p>
+              </div>
+              <div className="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+                <h6 className="text-uppercase fw-bold mb-4">Kapcsolat</h6>
+                <p><i className="fas fa-home me-3"></i> Budapest, 14.kerület, HU</p>
+                <p>
+                  <i className="fas fa-envelope me-3"></i>
+                  teautod@example.com
+                </p>
+                <p><i className="fas fa-phone me-3"></i> + 01 234 567 88</p>
+                <p><i className="fas fa-print me-3"></i> + 01 234 567 89</p>
+              </div>
+            </div>
+          </div>
+        </section>
+        <div className="text-center p-4">
+          © 2023 Copyright:
+          <Link to={'/'} className="text-reset fw-bold">TeAutod.hu</Link>
+        </div>
+      </footer>
+    );
+};
+export default FooterComponent;
---
+++ src/components/Navbar-component.tsx
@@ -0,0 +1,85 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import axios from 'axios';
+
+const NavbarComponent: React.FC = () => {
+  // const [token, setAuthToken] = useState<string | null>(localStorage.getItem('token'));
+
+  //  const handleLogout = async () => {
+  //   const token = localStorage.getItem('token');
+  //   if (token === null) return;
+  
+  //   const response = await fetch('http://localhost:3001/auth/logout', {
+  //     method: 'DELETE',
+  //     headers: {
+  //       'Authorization': `Bearer ${token}`
+  //     }
+  //   });
+    
+  //   if (response.ok) {
+  //     localStorage.removeItem('token');
+  //     setAuthToken(null);
+  //   }
+  // }
+
+
+const handleLogout = async () => {
+  const token = localStorage.getItem('authToken');
+  if (token === null) return;
+
+  try {
+    const response = await axios.delete('http://localhost:3001/auth/logout', {
+      headers: {
+        'Authorization': `Bearer ${token}`
+      }
+    });
+
+    if (response.status === 204) {
+      localStorage.removeItem('authToken');
+      window.location.href = "/";
+    }
+  } catch (error) {
+    // handle error
+  }
+}
+return (
+      <nav className="navbar navbar-expand-sm bg-light fixed-top">
+        <div className="container-fluid">
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <Link to={'/'} className="nav-link active" aria-current="page">Főoldal</Link>
+              </li>
+              <li className="nav-item">
+                <Link to={'/garage'} className="nav-link">Garázs</Link>
+              </li>
+              <li className="nav-item">
+                <Link to='./calendar' className="nav-link">Naptár</Link>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Rólunk</a>
+              </li>
+            </ul>
+            <ul className="navbar-nav ms-auto">
+              <div className="btn-group dropstart">
+                <button type="button" className="btn btn-light dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+                  <img className="img-fluid img-thumbnail nav-item" src={'bxs-user-circle.png'} alt="userProfile" title="User" />
+                </button>
+                <ul className="dropdown-menu">
+                  <li><h6 className="dropdown-header">Profil</h6></li>
+                  <li><Link to='/login' className="dropdown-item">Belépés</Link></li>
+                  <li><Link to='/register' className="dropdown-item">Regisztráció</Link></li>
+                  <li><a className="dropdown-item" href="#">Profil szerkesztése</a></li>
+                  <li><Link to={'/'} className="dropdown-item" onClick={handleLogout} >Kijelentkezés</Link></li>
+                </ul>
+              </div>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    );
+};
+export default NavbarComponent;
