c923591bea65896c42287f54bb561f21b10e7906
zmarc3ll <zubermarcellpetrik@gmail.com>
2023-04-02 14:23:40 +0200

    car pic on load, car dataa on load

--- src/components/pages/GarageForCar.tsx
+++ src/components/pages/GarageForCar.tsx
@@ -1,54 +1,100 @@
-import axios from "axios";
 import { Component } from "react";
 
-interface State {
-    selectedFile: File | null;
+interface carPictureResponse {
+    pictures: CarPicture[];
+}
+
+interface carListResponse {
+    cars: Car[];
+}
+
+interface CarPicture {
+    picId: number;
     carPic: string;
-    loading: boolean;
+    carsId: number;
+}
+
+interface Car {
+    carId: number;
+    brand: string;
+    model: string;
+    modelYear: number;
+    fuelType: string;
+    carPower: number;
+    gearType: string;
+    color: string;
+    chassisType: string;
+    doors: number;
+    fuelEconomy: string;
+    license_plate: string;
+    givenName: string;
+}
+
+interface State {
+    carPic: string;
+    cars: Car[];
+    pictures: CarPicture[];
+    carLoaded: boolean;
 }
 
 export default class GarageForCar extends Component<{}, State> {
 
     state: State = {
-        // Initially, no file is selected
-        selectedFile: null,
-        carPic: "",
-        loading: true,
-    };
-    onFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        // Update the state
-        this.setState({ selectedFile: event.target.files?.[0] || null });
-    };
+        carPic: '',
+        cars: [],
+        pictures: [],
+        carLoaded: false,
+    }
 
-    onFileUpload = () => {
-        // Create an object of formData
-        const formData = new FormData();
-
-        // Update the formData object
-        if (this.state.selectedFile) {
-            formData.append(
-                "carFile",
-                this.state.selectedFile,
-                this.state.selectedFile.name
-            );
-
-            // Details of the uploaded file
-
-            // Request made to the backend api
-            // Send formData object
-            axios.post("http://localhost:3001/uploadfile", formData).then((res) => {
-                // Set the carPic state to the filename returned by the server
-                this.setState({ carPic: res.data.carPic, loading: false });
-            }).catch((err) => {
-                console.log(err);
-                this.setState({ loading: false });
-            });
-            this.setState({ loading: true });
-        } else {
-            this.setState({ loading: false });
+    async loadCarPics() {
+        let userId = localStorage.getItem('userId');
+        let response = await fetch('http://localhost:3001/carPic/${userId}');
+        let responseUrl: string = response.url.substring(0, 29) + userId;
+        let responseOk = await fetch(responseUrl);
+        let data = await responseOk.json() as carPictureResponse;
+        if (responseOk.ok && Object.keys(data).length > 0) {
+            let datas = Object.values(data);
+            const carPic = datas[0].carPic;
+            this.setState({
+                carPic: carPic,
+            })
         }
-    };
+    }
+
+    async loadUsersCars() {
+        try {
+            const thisUserId = localStorage.getItem('userId');
+            let response = await fetch('http://localhost:3001/usersCar/${thisUserId}');
+            let responseUrl: string = response.url.substring(0, 31) + thisUserId;
+            let responseOk = await fetch(responseUrl);
+            if (!responseOk.ok) {
+                throw new Error('Network response was not ok');
+            }
+            let data = await responseOk.json() as carListResponse;
+            let datas = Object.values(data);
+            console.log('data', data)
+            this.setState({
+                cars: data.cars,
+                carLoaded: true,
+            });
+            console.log('thisstate:', typeof this.state.cars);
+            if (response === null) {
+                this.setState({
+                    carLoaded: false,
+                });
+            }
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        }
+    }
+
+    componentDidMount() {
+        this.loadUsersCars();
+        this.loadCarPics();
+    }
+
     render() {
+        const carsArray = Object.values(this.state.cars);
         return <body id="undoBlockContent">
             <div className="container-fluid" id="garageContainer">
                 <div className="row">
@@ -56,33 +102,49 @@ export default class GarageForCar extends Component<{}, State> {
                         <div className="card">
                             {/* cars picture */}
                             <div className="card-body">
-                                {this.state.loading ? (
-                                    <div>Loading...</div>
-                                ) : (
-                                    <img
-                                        src={`http://localhost:3001/uploadedFiles/cars/${this.state.carPic}`}
-                                        alt=""
-                                        className=" rounded shadow-lg bg-body ms-0 img-fluid"
-                                    />
-                                )}
-                                <div>
-                                    <input type="file" onChange={this.onFileChange} />
-                                    <button onClick={this.onFileUpload}>
-                                        Upload!
-                                    </button>
-                                </div>
+                                <img
+                                    src={`http://localhost:3001/uploadedFiles/cars/${this.state.carPic}`}
+                                    alt=""
+                                    className=" rounded shadow-lg bg-body ms-0 img-fluid"
+                                    id="carsImage"
+                                />
                             </div>
                         </div>
                         <div className="card mt-4">
                             <div className="card-body">
                                 {/* cars details */}
-                                <h4 className={'text-center pb-2'}>Az autó adatai:</h4>
+                                {this.state.cars.map((car: Car) => (
+                                        <h4 key={car.carId} className={'text-center pb-2'}>
+                                            <span><strong>{car.givenName} adatai:</strong></span>
+                                        </h4>
+                                    ))}
                                 <ul id="carDataList">
-                                    <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. </li>
-                                    <li>Deserunt, ut ducimus! Molestiae laboriosam, dolore rerum perspiciatis ducimus eaque inventore,</li>
-                                    <li>Dolor molestias quidem ex rerum similique, repellendus, ea amet doloribus obcaecati quas </li>
-                                    <li>Magnam earum inventore officia vero quo eos nemo provident molestias nostrum </li>
-                                    <li>Officiis voluptas molestias explicabo. Ut consequuntur omnis quas a doloribus deleniti </li>
+                                    <ul id="carDataList">
+                                        {this.state.cars.map((car: Car) => (
+                                            <><li key={car.carId}>
+                                                <span>Márka: {car.brand}</span><br />
+                                            </li><li key={car.carId}>
+                                                    <span>Modell: {car.model}</span><br />
+                                                </li><li key={car.carId}>
+                                                    <span>Évjárat: {car.modelYear}</span><br />
+                                                </li><li key={car.carId}>
+                                                    <span>Üzemanyag típusa: {car.fuelType}</span><br />
+                                                </li><li key={car.carId}>
+                                                    <span>Lóerő: {car.carPower}</span><br />
+                                                </li><li key={car.carId}>
+                                                    <span>Váltó típusa: {car.gearType}</span><br />
+                                                </li><li key={car.carId}><span>Szín: {car.color}</span><br />
+                                                </li><li key={car.carId}>
+                                                    <span>Kaszni típusa: {car.chassisType}</span><br />
+                                                </li><li key={car.carId}>
+                                                    <span>Ajtók száma: {car.doors}</span><br />
+                                                </li><li key={car.carId}>
+                                                    <span>Fogyasztás: {car.fuelEconomy}</span><br />
+                                                </li><li key={car.carId}>
+                                                    <span>Rendszám: {car.license_plate}</span><br />
+                                                </li></>
+                                        ))}
+                                    </ul>
                                 </ul>
                             </div>
                         </div>
